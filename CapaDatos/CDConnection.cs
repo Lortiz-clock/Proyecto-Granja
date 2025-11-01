413a58d3d6ee28862d54ebaaad638069eb1b836a
Lortiz <lortizh3@miumg.edu.gt>
2025-11-01 16:16:11 -0600

    Creacion de la base de datos

---
+++ CapaDatos/CDConnection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class CDConnection
+    {
+        private SqlConnection db_conexion = new SqlConnection("Data Source=DESKTOP-LVVM941\\SQLEXPRESS;Initial Catalog=db_GranjasSA;Integrated Security=True;Encrypt=False");
+        //private SqlConnection db_conexion = new SqlConnection("Data Source=USUARIO\\DAVID_SARMIENTOS;Initial Catalog=db_GranjasSA;Integrated Security=True;Encrypt=False");
+        public SqlConnection MtdAbrirConexion()
+        {
+            if (db_conexion.State == ConnectionState.Closed)
+            {
+                db_conexion.Open();
+            }
+            return db_conexion;
+        }
+
+        public SqlConnection MtdCerrarConexion()
+        {
+            if (db_conexion.State == ConnectionState.Open)
+            {
+                db_conexion.Close();
+            }
+            return db_conexion;
+        }
+    }
+}
