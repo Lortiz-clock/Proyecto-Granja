e54cca82c9adbaa91f58f7761a77eb8d9c6e2777
CrissM <cramirezm29@miumg.edu.gt>
2025-11-02 12:00:11 -0600

    se hace la creacion de menu, se agregan componentes logicos de las demas capas, sin embargo tambien se raliza los botones del sub menu

--- CapaLogica/Class1.cs
+++ CapaLogica/Class1.cs
@@ -8,5 +8,29 @@ namespace CapaLogica
 {
     public class Class1
     {
+
+        
+
+            public decimal CalcularTotal(int cantidad, decimal precioUnitario)
+            {
+                return cantidad * precioUnitario;
+            }
+            public decimal CalcularImpuesto(decimal total)
+            {
+                return total * 0.12m;
+            }
+            public decimal CalcularTotalVenta(decimal total, decimal impuesto)
+            {
+                return total + impuesto;
+            }
+
+
+
+            public decimal CalcularSalarioFinal(decimal salarioBase, decimal bonos, decimal descuentos)
+            {
+                return salarioBase + bonos - descuentos;
+            }
+        }
+
     }
-}
+
