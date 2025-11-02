a8ddfc5a3226885dbd5c80b3b988a6f25ad67f8d
DSarmientos <davidsarmientos1993@gmail.com>
2025-11-01 23:19:28 -0600

    Se agraga CLPagoVenta

---
+++ CapaLogica/ClPagoVenta.cs
@@ -0,0 +1,34 @@
+﻿using CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogica
+{
+    public class ClPagoVenta
+    {
+        CDConnection conn = new CDConnection();
+
+        public DateTime MtdFechaHoy()
+        {
+            return DateTime.Now;
+        }
+
+        public double MtdTotalPagoVenta(decimal CodigoPagoVenta)
+        {
+
+            SqlCommand cmd = new SqlCommand("Select isnull(sum(Monto),0) as Monto from tbl_PagosVentas where CodigoPagoVenta=@CodigoPagoVenta", conn.MtdAbrirConexion());
+            cmd.Parameters.AddWithValue("@CodigoPagoVenta", CodigoPagoVenta);
+
+            object resultado = cmd.ExecuteScalar();
+            conn.MtdCerrarConexion();
+
+            return resultado != null ? Convert.ToDouble(resultado) : 0;
+        }
+
+
+    }
+}
