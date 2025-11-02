1f33d2120a4be0b35d43c86f08ccd277ba48f84f
DSarmientos <davidsarmientos1993@gmail.com>
2025-11-01 23:11:30 -0600

    se agregan clases envios y clientes

---
+++ CapaDatos/CDClientes.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class CDClientes
+    {
+        CDConnection conn = new CDConnection();
+
+        public DateTime MtdFechaHoy()
+        {
+            return DateTime.Now;
+        }
+
+        public DataTable MtdConsultaClientes()
+        {
+            string QueryConsultaClientes = "select * from tbl_Clientes";
+            SqlDataAdapter sqlAdap = new SqlDataAdapter(QueryConsultaClientes, conn.MtdAbrirConexion());
+            DataTable dtClientes = new DataTable();
+            sqlAdap.Fill(dtClientes);
+            conn.MtdCerrarConexion();
+            return dtClientes;
+        }
+
+        public List<dynamic> MtdListaClientes()
+        {
+            List<dynamic> ListaClientes = new List<dynamic>();
+            string QueryListaClientes = "select * from tbl_Clientes";
+            SqlCommand lc = new SqlCommand(QueryListaClientes, conn.MtdAbrirConexion());
+            SqlDataReader reader = lc.ExecuteReader();
+
+            while (reader.Read())
+            {
+                ListaClientes.Add(new
+                {
+                    Value = reader["CodigoCliente"],
+                    Text = $"{reader["CodigoCliente"]} - {reader["NombreCliente"]}"
+                });
+            }
+            conn.MtdCerrarConexion();
+            return ListaClientes;
+        }
+
+
+
+        public string MtdListasClientesDgv(int CodigoCliente)
+        {
+            string resultado = string.Empty;
+            string QueryListaClientes = "select * from tbl_Clientes where CodigoCliente = @CodigoCliente";
+            SqlCommand cmd = new SqlCommand(QueryListaClientes, conn.MtdAbrirConexion());
+            cmd.Parameters.AddWithValue("@CodigoCliente", CodigoCliente);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+                string codigo = reader["CodigoCliente"].ToString();
+                string nombre = reader["NombreCliente"].ToString();
+                resultado = $"{codigo} - {nombre}";
+            }
+            else
+            {
+                resultado = string.Empty;
+            }
+            conn.MtdCerrarConexion();
+            return resultado;
+        }
+
+        //Metodo para carga de datos
+        public DataTable MtdCargarDatos()
+        {
+            string QueryCargarDatos = "select CodigoCliente,NombreCliente, TipoCliente,Telefono,Correo,Direccion,Estado from tbl_Clientes order by NombreCliente asc";
+            SqlDataAdapter SqlAdap = new SqlDataAdapter(QueryCargarDatos, conn.MtdAbrirConexion());
+            DataTable dtCargarDatos = new DataTable();
+            SqlAdap.Fill(dtCargarDatos);
+            conn.MtdCerrarConexion();
+            return dtCargarDatos;
+        }
+
+        //Metodo para Eliminar datos de la base de datos
+        public void MtdEliminarDatos(int CodigoCliente)
+        {
+            string QueryEliminarDatos = "delete tbl_Clientes where CodigoCliente = @CodigoCliente";
+            SqlCommand CommeEliminarDatos = new SqlCommand(QueryEliminarDatos, conn.MtdAbrirConexion());
+            CommeEliminarDatos.Parameters.AddWithValue("@CodigoCliente", CodigoCliente);
+            CommeEliminarDatos.ExecuteNonQuery();
+            conn.MtdCerrarConexion();
+        }
+        //Metodo para Agregar datos de la base de datos
+        public void MtdAgregarDatos(string NombreCliente, string TipoCliente, string Telefono, string Correo, string Direccion, string Estado)
+        {
+            string QueryAgregarDatos = "insert into tbl_Clientes(NombreCliente,TipoCliente,Telefono,Correo,Direccion,Estado) values (@NombreCliente,@TipoCliente,@Telefono,@Correo,@Direccion,@Estado)";
+            SqlCommand CommeAgregarDatos = new SqlCommand(QueryAgregarDatos, conn.MtdAbrirConexion());
+            CommeAgregarDatos.Parameters.AddWithValue("@NombreCliente", NombreCliente);
+            CommeAgregarDatos.Parameters.AddWithValue("@TipoCliente", TipoCliente);
+            CommeAgregarDatos.Parameters.AddWithValue("@Telefono", Telefono);
+            CommeAgregarDatos.Parameters.AddWithValue("@Correo", Correo);
+            CommeAgregarDatos.Parameters.AddWithValue("@Direccion", Direccion);
+            CommeAgregarDatos.Parameters.AddWithValue("@Estado", Estado);
+            CommeAgregarDatos.ExecuteNonQuery();
+            conn.MtdCerrarConexion();
+        }
+        //Metodo para Editar datos de la base de datos
+        public void MtdEditarDatos(int CodigoCliente, string NombreCliente, string TipoCliente, string Telefono, string Correo, string Direccion, string Estado)
+        {
+            string QueryEditarDatos = "update tbl_Clientes set NombreCliente = @NombreCliente,TipoCliente = @TipoCliente,Telefono = @Telefono,Correo = @Correo,Direccion = @Direccion,Estado = @Estado where  CodigoCliente = @CodigoCliente";
+            SqlCommand CommeEditarDatos = new SqlCommand(QueryEditarDatos, conn.MtdAbrirConexion());
+            CommeEditarDatos.Parameters.AddWithValue("@CodigoCliente", CodigoCliente);
+            CommeEditarDatos.Parameters.AddWithValue("@NombreCliente", NombreCliente);
+            CommeEditarDatos.Parameters.AddWithValue("@TipoCliente", TipoCliente);
+            CommeEditarDatos.Parameters.AddWithValue("@Telefono", Telefono);
+            CommeEditarDatos.Parameters.AddWithValue("@Correo", Correo);
+            CommeEditarDatos.Parameters.AddWithValue("@Direccion", Direccion);
+            CommeEditarDatos.Parameters.AddWithValue("@Estado", Estado);
+            CommeEditarDatos.ExecuteNonQuery();
+            conn.MtdCerrarConexion();
+        }
+    }
+}
---
+++ CapaDatos/CDEnvios.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class CDEnvios
+    {
+        CDConnection conn = new CDConnection();
+
+        public DateTime MtdFechaHoy()
+        {
+            return DateTime.Now;
+        }
+
+
+        public DataTable MtdConsultaEnvios()
+        {
+            string QueryConsultaEnvios = "select * from tbl_Ventas";
+            SqlDataAdapter sqlAdap = new SqlDataAdapter(QueryConsultaEnvios, conn.MtdAbrirConexion());
+            DataTable dtEnvios = new DataTable();
+            sqlAdap.Fill(dtEnvios);
+            conn.MtdCerrarConexion();
+            return dtEnvios;
+        }
+
+        public List<dynamic> MtdListaEnvios()
+        {
+            List<dynamic> ListaEnvios = new List<dynamic>();
+            string QueryListaEnvios = "select * from tbl_Envios";
+            SqlCommand le = new SqlCommand(QueryListaEnvios, conn.MtdAbrirConexion());
+            SqlDataReader reader = le.ExecuteReader();
+
+            while (reader.Read())
+            {
+                ListaEnvios.Add(new
+                {
+                    Value = reader["CodigoEnvio"]
+
+                });
+            }
+            conn.MtdCerrarConexion();
+            return ListaEnvios;
+        }
+
+        public DataTable MtdCargarDatos()
+        {
+            string QueryCargarDatos = "select CodigoEnvio,CodigoVenta, FechaEnvio,DireccionEnvio,TipoTransporte,PlacaTransporte,Observacion,Estado from tbl_Envios order by FechaEnvio asc";
+            SqlDataAdapter SqlAdap = new SqlDataAdapter(QueryCargarDatos, conn.MtdAbrirConexion());
+            DataTable dtCargarDatos = new DataTable();
+            SqlAdap.Fill(dtCargarDatos);
+            conn.MtdCerrarConexion();
+            return dtCargarDatos;
+        }
+
+        public string MtdListasEnviosDgv(int CodigoEnvio)
+        {
+            string resultado = string.Empty;
+            string QueryListaEnvios = "select * from tbl_Envios where CodigoEnvio = @CodigoEnvio";
+            SqlCommand cmd = new SqlCommand(QueryListaEnvios, conn.MtdAbrirConexion());
+            cmd.Parameters.AddWithValue("@CodigoEnvio", CodigoEnvio);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+                string codigo = reader["CodigoEnvio"].ToString();
+
+                resultado = $"{codigo}";
+            }
+            else
+            {
+                resultado = string.Empty;
+            }
+            conn.MtdCerrarConexion();
+            return resultado;
+        }
+
+        public void MtdAgregarEnvio(int CodigoVenta, DateTime FechaEnvio, string DireccionEnvio, string TipoTransporte, string PlacaTransporte, string Observacion, string Estado)
+        {
+            string QueryAgregarEnvio = "insert into tbl_Envios(CodigoVenta, FechaEnvio, DireccionEnvio, TipoTransporte,PlacaTransporte,Observacion, Estado)  values(@CodigoVenta, @FechaEnvio, @DireccionEnvio, @TipoTransporte,@PlacaTransporte,@Observacion, @Estado)";
+            SqlCommand commeAgregarEnvio = new SqlCommand(QueryAgregarEnvio, conn.MtdAbrirConexion());
+            commeAgregarEnvio.Parameters.AddWithValue("@CodigoVenta", CodigoVenta);
+            commeAgregarEnvio.Parameters.AddWithValue("@FechaEnvio", FechaEnvio);
+            commeAgregarEnvio.Parameters.AddWithValue("@DireccionEnvio", DireccionEnvio);
+            commeAgregarEnvio.Parameters.AddWithValue("@TipoTransporte", TipoTransporte);
+            commeAgregarEnvio.Parameters.AddWithValue("@PlacaTransporte", PlacaTransporte);
+            commeAgregarEnvio.Parameters.AddWithValue("@Observacion", Observacion);
+            commeAgregarEnvio.Parameters.AddWithValue("@Estado", Estado);
+            commeAgregarEnvio.ExecuteNonQuery();
+            conn.MtdCerrarConexion();
+        }
+
+
+        public void MtdActualizarEnvio(int CodigoEnvio, int CodigoVenta, DateTime FechaEnvio, string DireccionEnvio, string TipoTransporte, string PlacaTransporte, string Observacion, string Estado)
+        {
+            string QueryActualizarEnvio = "update tbl_Envios set CodigoVenta = @CodigoVenta,FechaEnvio = @FechaEnvio,DireccionEnvio = @DireccionEnvio,TipoTransporte = @TipoTransporte,PlacaTransporte = @PlacaTransporte,Observacion = @Observacion,Estado = @Estado where CodigoEnvio = @CodigoEnvio";
+            SqlCommand commeActualizarEnvio = new SqlCommand(QueryActualizarEnvio, conn.MtdAbrirConexion());
+            commeActualizarEnvio.Parameters.AddWithValue("@CodigoEnvio", CodigoEnvio);
+            commeActualizarEnvio.Parameters.AddWithValue("@CodigoVenta", CodigoVenta);
+            commeActualizarEnvio.Parameters.AddWithValue("@FechaEnvio", FechaEnvio);
+            commeActualizarEnvio.Parameters.AddWithValue("@DireccionEnvio", DireccionEnvio);
+            commeActualizarEnvio.Parameters.AddWithValue("@TipoTransporte", TipoTransporte);
+            commeActualizarEnvio.Parameters.AddWithValue("@PlacaTransporte", PlacaTransporte);
+            commeActualizarEnvio.Parameters.AddWithValue("@Observacion", Observacion);
+            commeActualizarEnvio.Parameters.AddWithValue("@Estado", Estado);
+            commeActualizarEnvio.ExecuteNonQuery();
+            conn.MtdCerrarConexion();
+        }
+
+        public void MtdEliminarEnvios(int CodigoEnvio)
+        {
+            string QueryEliminarEnvios = "delete tbl_Envios where CodigoEnvio = @CodigoEnvio";
+            SqlCommand commeEliminarEnvios = new SqlCommand(QueryEliminarEnvios, conn.MtdAbrirConexion());
+            commeEliminarEnvios.Parameters.AddWithValue("@CodigoEnvio", CodigoEnvio);
+            commeEliminarEnvios.ExecuteNonQuery();
+            conn.MtdCerrarConexion();
+        }
+
+
+
+    }
+}
