586c1827d1570786b9702b63f553b1b380d922ef
Lortiz <lortizh3@miumg.edu.gt>
2025-11-01 22:51:37 -0600

    prueva

--- CapaPresentacion/Program.cs
+++ CapaPresentacion/Program.cs
@@ -18,6 +18,6 @@ namespace CapaPresentacion
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new CapaPresentacion.FormRoles());
             //Application.Run(new FormGranja());
-        }
+        }   
     }
 }
